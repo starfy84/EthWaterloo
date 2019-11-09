f0ba47307f0f72fd85ebe4b96bde8fc2b9aeb8c4
advenia <august.lin1320@gmail.com>
2019-11-09 12:05:47 -0500

    reinitialize truffle project

--- contracts/TodoList.sol
+++
@@ -1,9 +0,0 @@
-//version
-pragma solidity ^0.5.0;
-
-//declare smart contract
-contract TodoList{
-    //keep track of the number of tasks inside the todo list
-    uint public taskCount = 0; //readable state variable
-}
-
