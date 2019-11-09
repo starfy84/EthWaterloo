1155a690f2224d8c11cb83642c5095c9c120154f
advenia <august.lin1320@gmail.com>
2019-11-09 12:09:27 -0500

    TodoList smart contract

---
+++ contracts/TodoList.sol
@@ -0,0 +1,6 @@
+pragma solidity ^0.5.0;
+
+contract TodoList{
+    // Number of tasks in the todo list
+    uint public taskCount = 0; // State variable written to blockchain; Similar to class variables in OOP
+}
