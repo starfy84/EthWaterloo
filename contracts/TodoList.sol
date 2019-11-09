1414917ba26ee3605d54c2a512c472d5398e3cb1
advenia <august.lin1320@gmail.com>
2019-11-09 11:57:57 -0500

    setup smart contract and truffle integration

---
+++ contracts/TodoList.sol
@@ -0,0 +1,9 @@
+//version
+pragma solidity ^0.5.0;
+
+//declare smart contract
+contract TodoList{
+    //keep track of the number of tasks inside the todo list
+    uint public taskCount = 0; //readable state variable
+}
+
